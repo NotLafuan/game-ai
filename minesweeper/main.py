4dfa56a640ec4637298aa792aad993d51a572571
Superstone1235 <naufalbakri122@gmail.com>
2023-03-01 10:22:44 +0800

    Cleanup

--- minesweeper/main.py
+++ minesweeper/main.py
@@ -1,29 +1,46 @@
 import itertools
+import random
 import time
 import pyautogui
-import ctypes
 import cv2
 import numpy as np
 import os
-
-
-def GetWindowRectFromName(name: str) -> tuple:
-    hwnd = ctypes.windll.user32.FindWindowW(0, name)
-    rect = ctypes.wintypes.RECT()
-    ctypes.windll.user32.GetWindowRect(hwnd, ctypes.pointer(rect))
-    return rect
+from utils import *
 
 
 class Reader:
-    def __init__(self, width: int = 9, height: int = 9) -> None:
-        self.width = width
-        self.height = height
+    def __init__(self) -> None:
         self.templates: list[cv2.Mat] = [cv2.imread(os.path.join('tiles', file))
                                          for file in os.listdir('tiles')]
+        self.find_width_height()
+
+    def find_width_height(self) -> None:
+        rect = get_window_rect_from_name('Minesweeper')
+        img = pyautogui.screenshot()
+        img = np.array(img)
+        img = img[:, :, ::-1].copy()  # RGB2BGR
+        window = img[rect.top:rect.bottom, rect.left:rect.right]
+        x, y = 23, 152
+        self.width: int = 0
+        self.height: int = 0
+        while True:  # for width
+            image = window[y:y+24, x+(self.width*24):x+(self.width*24)+24]
+            try:
+                mse(self.templates[0], image)
+                self.width += 1
+            except:
+                break
+        while True:  # for height
+            image = window[y+(self.height*24):y+(self.height*24)+24, x:x+24]
+            try:
+                mse(self.templates[0], image)
+                self.height += 1
+            except:
+                break
 
     @property
     def tile_images(self) -> list[list[cv2.Mat]]:
-        rect = GetWindowRectFromName('Minesweeper')
+        rect = get_window_rect_from_name('Minesweeper')
         img = pyautogui.screenshot()
         img = np.array(img)
         img = img[:, :, ::-1].copy()  # RGB2BGR
@@ -38,33 +55,13 @@ class Reader:
             tiles.append(row)
         return tiles
 
-    def mse(self, imageA, imageB) -> float:
-        # the 'Mean Squared Error' between the two images is the
-        # sum of the squared difference between the two images;
-        # NOTE: the two images must have the same dimension
-        err = np.sum((imageA.astype('float') - imageB.astype('float')) ** 2)
-        err /= float(imageA.shape[0] * imageA.shape[1])
-
-        # return the MSE, the lower the error, the more "similar"
-        # the two images are
-        return err
-
-    def compare_tile(self, tile: cv2.Mat) -> int:
-        lowest = np.inf
-        lowest_index = 0
-        for i, template in enumerate(self.templates):
-            if (result := self.mse(template, tile)) < lowest:
-                lowest = result
-                lowest_index = i
-        return lowest_index-1
-
     @property
     def tiles(self) -> list[list[int]]:
         tiles = []
         for row in self.tile_images:
             tile_row = []
             for tile_image in row:
-                tile_row.append(self.compare_tile(tile_image))
+                tile_row.append(compare_image(tile_image, self.templates))
             tiles.append(tile_row)
         return tiles
 
@@ -89,85 +86,33 @@ class Reader:
         print(text, end='', flush=True)
 
 
-class AI:
-    def __init__(self, reader: Reader):
-        self.reader = reader
-        self.read_tiles()
-        self._opened_tiles = []
+class AI(Reader):
+    def __init__(self) -> None:
+        super().__init__()
+        self._number_tiles = []
         self._guess_tiles = []
-        self.flagged_tiles = []
+        self.read_tiles()
 
     def read_tiles(self) -> None:
-        self.board: list[list[int]] = self.reader.tiles.copy()
+        time.sleep(0.1)
+        self.board: list[list[int]] = self.tiles
 
-    def click(self, coords: list[tuple[int, int]]):
-        rect = GetWindowRectFromName('Minesweeper')
-        x1, y1 = rect.left+23, rect.top+152
-        for x, y in coords:
-            pyautogui.moveTo(x1+(x*24)+12, y1+(y*24)+12)
-            pyautogui.click()
-
-    def surround_opened(self, x: int, y: int):
-        possible_tiles = list(itertools.product([-1, 0, 1], repeat=2))
-        for i, j in possible_tiles:
+    def number_neighbour(self, x: int, y: int) -> bool:
+        neighbour_tiles = list(itertools.product([-1, 0, 1], repeat=2))
+        for i, j in neighbour_tiles:
             try:
-                if self.board[y+j][x+i] > 0:
+                j = y+j
+                i = x+i
+                if j < 0 or i < 0:
+                    continue
+                if self.board[j][i] > 0 and self.board[j][i] < 9:
                     return True
             except:
                 pass
-
-    def distance(self, A: tuple[int, int], B: tuple[int, int]):
-        pointA = np.array(A)
-        pointB = np.array(B)
-        sum_sq = np.sum(np.square(pointA - pointB))
-        return np.sqrt(sum_sq)
-
-    @property
-    def guess_tiles(self) -> list[tuple[int, int]]:
-        if not self._guess_tiles:
-            lst = []
-            for j in range(self.reader.height):
-                for i in range(self.reader.width):
-                    if self.board[j][i] == -1:
-                        if self.surround_opened(i, j):
-                            lst.append((i, j))
-            if not lst:
-                return lst
-            # # sort by distance
-            # new_lst = [lst.pop(0)]
-            # while lst:
-            #     shortest = np.inf
-            #     shortest_coord_index = 0
-            #     for i, coord in enumerate(lst):
-            #         if (distance := self.distance(new_lst[-1], coord)) < shortest:
-            #             shortest = distance
-            #             shortest_coord_index = i
-            #     new_lst.append(lst.pop(shortest_coord_index))
-            # # take flags to the back
-            # print(new_lst)
-            # for i, coord in enumerate(new_lst):
-            #     if coord in self.flagged_tiles:
-            #         # new_lst.insert(0, new_lst.pop(i))
-            #         new_lst.append(new_lst.pop(i))
-            # print()
-            # print(new_lst)
-            # replace value
-            self._guess_tiles = lst
-        return self._guess_tiles
-
-    @property
-    def opened_tiles(self) -> list[tuple[int, int]]:
-        if not self._opened_tiles:
-            lst = []
-            for j in range(self.reader.height):
-                for i in range(self.reader.width):
-                    if self.board[j][i] > 0:
-                        lst.append((i, j))
-            self._opened_tiles = lst
-        return self._opened_tiles
+        return False
 
     def clear_properties(self) -> None:
-        self._opened_tiles = []
+        self._number_tiles = []
         self._guess_tiles = []
 
     def surround_coords(self, x: int, y: int) -> list[tuple[int, int]]:
@@ -177,132 +122,160 @@ class AI:
         return lst
 
     def test_bombs(self, bombs: list[int | None]) -> bool:
-        for x, y in self.opened_tiles:
+        for x, y in self.number_tiles:
             surround_bombs = []
             for coords in self.surround_coords(x, y):
                 if coords in self.guess_tiles:
                     index = self.guess_tiles.index(coords)
                     surround_bombs.append(bombs[index])
-            if surround_bombs.count(1) > self.board[y][x]:
+                try:
+                    if coords[1] < 0 or coords[0] < 0:
+                        continue
+                    if self.board[coords[1]][coords[0]] == 11:
+                        surround_bombs.append(1)
+                except IndexError:
+                    pass
+            if (surround_bombs.count(1)) > self.board[y][x]:
                 return False
             if (surround_bombs.count(None) + surround_bombs.count(1)) < self.board[y][x]:
                 return False
         return True
 
-    def add_flags(self, possible_bombs: list[int]) -> None:
-        for i, bomb in enumerate(possible_bombs):
-            if bomb == 1:
-                if self.guess_tiles[i] not in self.flagged_tiles:
-                    self.flagged_tiles.append(self.guess_tiles[i])
-
-    def get_flag(self) -> list[int]:
+    @property
+    def number_tiles(self) -> list[tuple[int, int]]:
+        if self._number_tiles:
+            return self._number_tiles
+        self.read_tiles()
         lst = []
-        for guess_tile in self.guess_tiles:
-            if guess_tile in self.flagged_tiles:
-                lst.append(1)
-            else:
-                lst.append(0)
-        return lst
+        for j in range(self.height):
+            for i in range(self.width):
+                if self.board[j][i] > 0 and self.board[j][i] < 9:
+                    lst.append((i, j))
+        self._number_tiles = lst
+        return self._number_tiles
+
+    @property
+    def guess_tiles(self) -> list[tuple[int, int]]:
+        if self._guess_tiles:
+            return self._guess_tiles
+        self.read_tiles()
+        lst = []
+        for j in range(self.height):
+            for i in range(self.width):
+                if self.board[j][i] == -1:
+                    if self.number_neighbour(i, j):
+                        lst.append((i, j))
+        self._guess_tiles = lst
+        return self._guess_tiles
 
     @property
     def possible_bombs(self) -> np.ndarray:
-        lst = []
         self.clear_properties()
-        turn = 0
-        flags = self.get_flag()
+        lst = []
         bombs = [None]*len(self.guess_tiles)
         used_bombs = [[] for _ in range(len(self.guess_tiles))]
-        has_reached_end = False
-        while not (len(used_bombs[0]) == 2 and not turn and has_reached_end):
-            if turn == len(self.guess_tiles):
-                turn -= 1  # go back one step
-                used_bombs[turn] = []
-                bombs[turn] = None
-                turn -= 1  # go back one more step
-            if flags[turn] == 1:
-                used_bombs[turn] = [0, 1]
-                bombs[turn] = 1
-                if self.test_bombs(bombs):
-                    turn += 1
-                else:
-                    bombs[turn] = None
-            else:
-                # check for unused number
-                number_to_use = -1
-                for choice in [0, 1]:
-                    if choice not in used_bombs[turn]:
-                        number_to_use = choice
-                        break
-                # no other unused number
-                if number_to_use < 0:
+        turn = 0
+        while turn != -1:
+            try:
+                if turn == len(self.guess_tiles):  # index out for range
+                    turn -= 1  # go back one step
                     used_bombs[turn] = []
                     bombs[turn] = None
-                    turn -= 1  # go back one step
-                    while flags[turn] == 1:
+                    turn -= 1  # go back one more step
+                else:
+                    # check for unused number
+                    number_to_use = -1
+                    for choice in [0, 1]:
+                        if choice not in used_bombs[turn]:
+                            number_to_use = choice
+                            break
+                    # no other unused number
+                    if number_to_use < 0:
                         used_bombs[turn] = []
                         bombs[turn] = None
                         turn -= 1  # go back one step
-                # test a number
-                else:
-                    bombs[turn] = number_to_use
-                    used_bombs[turn].append(number_to_use)
-                    if self.test_bombs(bombs):
-                        turn += 1
+                    # test the number
                     else:
-                        bombs[turn] = None
-            if None not in bombs and self.test_bombs(bombs):
-                lst.append(bombs.copy())
-                if not has_reached_end:
-                    has_reached_end = True
-        return np.array(lst).mean(axis=0)
-
-    @property
-    def possible_bombs_bruteforce(self) -> np.ndarray:
-        lst = []
-        self.clear_properties()
-        for bombs in list(itertools.product([0, 1], repeat=len(self.guess_tiles))):
-            if self.test_bombs(bombs, self.opened_tiles, self.guess_tiles):
-                lst.append(bombs)
-                print(bombs)
+                        bombs[turn] = number_to_use
+                        used_bombs[turn].append(number_to_use)
+                        if self.test_bombs(bombs):
+                            turn += 1
+                        else:
+                            bombs[turn] = None
+                if None not in bombs and self.test_bombs(bombs):
+                    lst.append(bombs.copy())
+            except KeyboardInterrupt:
+                print('KeyboardInterrupt')
+                time.sleep(0.5)
+                pyautogui.hotkey('alt', 'tab')
+                time.sleep(0.1)
+                break
+        print('possiblities', len(lst))
+        if not lst:
+            raise IndexError
         return np.array(lst).mean(axis=0)
 
     def choose_and_click(self):
-        time.sleep(0.1)
-        self.read_tiles()
-        if not self.opened_tiles:
-            # self.click([(random.randint(0, self.reader.width-1),
-            #            random.randint(0, self.reader.height-1))])
-            self.click([(1, 1)])
+        if not self.number_tiles:
+            click([(random.randint(0, self.width-1),
+                    random.randint(0, self.height-1))])
+            self.clear_properties()
         else:
             possible_bombs = self.possible_bombs.copy()
-            self.add_flags(possible_bombs)
             print(possible_bombs)
+            if 1 in possible_bombs:
+                coords = []
+                for i in np.where(possible_bombs == 1)[0]:
+                    coords.append(self.guess_tiles[i])
+                right_click(coords)
             if 0 in possible_bombs:
                 coords = []
                 for i in np.where(possible_bombs == 0)[0]:
                     coords.append(self.guess_tiles[i])
-                self.click(coords)
+                click(coords)
             else:
                 i = np.argmin(possible_bombs)
-                self.click([self.guess_tiles[i]])
+                click([self.guess_tiles[i]])
+
+    def print_guess(self, include_spaces: bool = True) -> None:
+        text = ''
+        for y, row in enumerate(self.tiles):
+            for x, tile in enumerate(row):
+                if (x, y) in self.guess_tiles:
+                    text += f'O '
+                elif tile == 0:
+                    text += f'  '
+                elif tile == -1:
+                    text += f'X '
+                elif tile == 9 or tile == 10:
+                    text += f'B '
+                elif tile == 11:
+                    text += f'F '
+                else:
+                    text += f'{tile} '
+            text += '\n'
+        if include_spaces:
+            text += '\n' * (os.get_terminal_size().lines -
+                            text.count('\n') - 1)
+        print(text, end='', flush=True)
 
 
 if __name__ == '__main__':
+    pyautogui.PAUSE = 0.0001
     while True:
         question = input()
         if question in ['q', 'Q']:
             break
         os.system('cls')
+        ai = AI()
         try:
-            pyautogui.PAUSE = 0.001
             pyautogui.hotkey('alt', 'tab')
-            time.sleep(1)
-            # reader = Reader(width=9, height=9)
-            reader = Reader(width=16, height=16)
-            # reader = Reader(width=30, height=16)
+            time.sleep(0.5)
             while True:
-                ai = AI(reader)
-                ai.reader.print_tiles(False)
+                ai.print_tiles(include_spaces=False)
                 ai.choose_and_click()
+                if any(9 in tile for tile in ai.board):
+                    print('BOMB!')
+                    break
         except IndexError:
-            pass
+            print('IndexError')
---
+++ minesweeper/utils/__init__.py
@@ -0,0 +1,55 @@
+import ctypes
+import cv2
+import numpy as np
+import pyautogui
+
+
+def get_window_rect_from_name(name: str):
+    hwnd = ctypes.windll.user32.FindWindowW(0, name)
+    rect = ctypes.wintypes.RECT()
+    ctypes.windll.user32.GetWindowRect(hwnd, ctypes.pointer(rect))
+    return rect
+
+
+def mse(imageA: cv2.Mat, imageB: cv2.Mat) -> float:
+    '''The 'Mean Squared Error' between the two images is the
+       sum of the squared difference between the two images;
+       NOTE: the two images must have the same dimension'''
+    err = np.sum((imageA.astype('float') - imageB.astype('float')) ** 2)
+    err /= float(imageA.shape[0] * imageA.shape[1])
+
+    # return the MSE, the lower the error, the more "similar"
+    return err
+
+
+def compare_image(image: cv2.Mat, templates: list[cv2.Mat]) -> int:
+    '''Return the index of the closest image in templates'''
+    lowest = np.inf
+    lowest_index = 0
+    for i, template in enumerate(templates):
+        if (result := mse(template, image)) < lowest:
+            lowest = result
+            lowest_index = i
+    return lowest_index-1
+
+
+def click(coords: list[tuple[int, int]]):
+    rect = get_window_rect_from_name('Minesweeper')
+    x1, y1 = rect.left+23, rect.top+152
+    for x, y in coords:
+        pyautogui.moveTo(x1+(x*24)+12, y1+(y*24)+12)
+        pyautogui.click()
+
+def right_click(coords: list[tuple[int, int]]):
+    rect = get_window_rect_from_name('Minesweeper')
+    x1, y1 = rect.left+23, rect.top+152
+    for x, y in coords:
+        pyautogui.moveTo(x1+(x*24)+12, y1+(y*24)+12)
+        pyautogui.rightClick()
+
+
+def distance(A: tuple[int, int], B: tuple[int, int]):
+    pointA = np.array(A)
+    pointB = np.array(B)
+    sum_sq = np.sum(np.square(pointA - pointB))
+    return np.sqrt(sum_sq)
